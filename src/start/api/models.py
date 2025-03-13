21822dff2eba41e894f8611170e098368713cd9f
qe-present <2664481691@qq.com>
2025-03-14 00:49:16 +0800

    add

---
+++ src/start/api/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+# Create your models here.
+class User(AbstractUser):
+    pass
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    stock = models.PositiveIntegerField()
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+    def __str__(self):
+        return self.name
+class  Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = 'PENDING'
+        COMPLETE = 'COMPLETE'
+        CANCELLED = 'CANCELLED'
+    order_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_at=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=20,
+                            choices=StatusChoices.choices,
+                            default=StatusChoices.PENDING)
+    product=models.ManyToManyField(Product, through='OrderItem', related_name='orders')
+    def __str__(self):
+        return f'Order {self.order_id} by {self.user.username}'
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField()
+    @property
+    def total(self):
+        return self.product.price * self.quantity
+    def __str__(self):
+        return f'{self.product.name} x {self.quantity} in order {self.order.order_id}'
---
+++ src/video1-Serializers - Response objects-Browsable API/api/views.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
+from .serializers import ProductSerializer,Product
+@api_view(['GET'])
+def product_list(request):
+    products=Product.objects.all()
+    serializer=ProductSerializer(products,many=True)
+    return  Response(
+        data=serializer.data
+
+    )
+@api_view(['GET'])
+def product_detail(request,pk):
+    product=get_object_or_404(Product,pk=pk)
+    serializer=ProductSerializer(product)
+    return Response(
+        data=serializer.data
+    )
